388a2babf8ac709c23912ac5a22b7c4ba33d5664
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-04-24 09:56:10 -0300

    TimeOut + Math MudaCor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 //Recuperando o botão com o getElementById
-const botao = document.getElementById("btn-1");
+// const botao = document.getElementById("btn-1");
 
 //Recuperar um item especifico com a
 //função querySelector(parâmetro)
@@ -9,16 +9,16 @@ const botao = document.getElementById("btn-1");
 
 //Recuerar uma lista de elementos com a função
 //querySelecorAll
-const seletores = document.querySelectorAll("li");
+// const seletores = document.querySelectorAll("li");
  
-seletores.forEach((item)=>{
+// seletores.forEach((item)=>{
 
-    if (item.textContent == "Item-15") {
-        let meuItem = item;
-        console.log(`Item selecionado : ${meuItem.textContent}`);
-        meuItem.textContent = "TÁ DOMINADO!!"   
-    }
-})
+//     if (item.textContent == "Item-15") {
+//         let meuItem = item;
+//         console.log(`Item selecionado : ${meuItem.textContent}`);
+//         meuItem.textContent = "TÁ DOMINADO!!"   
+//     }
+// })
 
 //Para estudar
 //setTimeOut
@@ -28,6 +28,25 @@ seletores.forEach((item)=>{
 //Math.Random
 //Math.Floor
 
+let tmp = "";
+
+function mudarCor() {
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    const cabecalho = document.querySelector(".cabecalho");
+    cabecalho.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    tmp = setTimeout(mudarCor, 1000);
+}
+
+mudarCor();
 
 
 
