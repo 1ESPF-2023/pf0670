ff4860a5b2e5c1fa915496273eca5de77a302f57
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-05-22 09:03:44 -0300

    Criação de obj + el msg no login / val form.

--- js/login.js
+++ js/login.js
@@ -1,26 +1,43 @@
 "use strict";
 
-let usuario = "";
-let senha = "";
-
-const botaoSubmit = document.querySelector("#btnSubmit");
-
-botaoSubmit.addEventListener("click",()=>{
-
+//Listener Global
+addEventListener("click", (evt)=>{
+    
     const inputUser = document.querySelector("input[name='nmUser']");
     const inputPass = document.querySelector("input[name='pass']");
-
+    
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
-
-    if(inputUser.value.length > 3 && inputPass.value.length > 3){
-        // inputUser.setAttribute("style", "outline-color:#00ff00;")
-        // inputPass.setAttribute("style", "outline-color:#00ff00;")
-        labelUser.setAttribute("style","color:#00ff00;");
-        labelPass.setAttribute("style","color:#00ff00;");
-    }else{
-        labelUser.setAttribute("style","color:#ff0000;");
-        labelPass.setAttribute("style","color:#ff0000;");
+    
+    //Recuperar o conteúdo dos campos e adicionar em um OBJETO
+    //CRIANDO O OBJETO USUARIO-LOGADO
+    console.log("Criou o obj!");
+    const usuarioLogado = {
+        nomeUsuarioLogado : inputUser.value,
+        senhaUsuarioLogado: inputPass.value
     }
+
+    if(evt.target.id == "btnSubmit"){
+        
+        if(inputUser.value.length > 3 && inputPass.value.length > 3){
+            labelUser.setAttribute("style","color:#00ff00;");
+            labelPass.setAttribute("style","color:#00ff00;");
+
+            //Se preencheu a qtd de caractéres necessária agora vamos validar o nomeUsuário e a senhaUsuário
+             if(usuarioLogado.nomeUsuarioLogado == "tutu" && usuarioLogado.senhaUsuarioLogado == "123456"){
+                alert("VALIDADO!");
+             }else{
+                alert("NÃO VALIDADO!");
+             }
+
+        }else{
+            labelUser.setAttribute("style","color:#ff0000;");
+            labelPass.setAttribute("style","color:#ff0000;");
+        }
+
+
+
+   }
+
 
 });
